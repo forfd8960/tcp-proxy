2043438c8765de4ea990d60b09eb1502892219c5
forfd8960 <forfd8960@gmail.com>
2024-10-23 22:24:25 +0800

    fix: split read and write and join read and write

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tcp-proxy
+
+## Run Proxy
+
+```sh
+listening on: localhost:8686
+proxy tcp to: localhost:8888
+proxy tcp to: localhost:8888
+copy 235 from Ok([::1]:8686)
+copy 211 from Ok(127.0.0.1:64559) to Ok([::1]:8686)
+done proxy
+```
--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,18 @@ async fn main() -> anyhow::Result<()> {
     let remote_addr = &args[2];
     let listener = TcpListener::bind(&addr).await?;
 
+    println!("listening on: {}", addr);
+
     loop {
         let remote_clone = remote_addr.clone();
         let (socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
             match proxy(socket, remote_clone).await {
-                Ok(_) => {}
+                Ok(_) => {
+                    println!("done proxy");
+                    return;
+                }
                 Err(e) => {
                     println!("proxy err: {}", e);
                 }
@@ -30,16 +35,25 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn proxy(mut socket: TcpStream, remote: String) -> anyhow::Result<()> {
-    let mut remote_socket = TcpStream::connect(&remote).await?;
+    println!("proxy tcp to: {}", remote);
 
-    match tokio::io::copy(&mut socket, &mut remote_socket).await {
+    let mut remote_socket = TcpStream::connect(&remote).await?;
+    let (mut r_read, mut r_write) = remote_socket.split();
+
+    let (mut s_read, mut s_write) = socket.split();
+    let (r1, r2) = tokio::join!(
+        tokio::io::copy(&mut s_read, &mut r_write),
+        tokio::io::copy(&mut r_read, &mut s_write)
+    );
+
+    match r1 {
         Ok(n) => {
             println!("copy {} from {:?}", n, socket.local_addr())
         }
         Err(e) => bail!("{}", e),
     }
 
-    match tokio::io::copy(&mut remote_socket, &mut socket).await {
+    match r2 {
         Ok(n) => {
             println!(
                 "copy {} from {:?} to {:?}",
